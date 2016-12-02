fd5e4296e0d9be198bd753f2ef09afc98f539ef3
Dawid Tomasiewicz <datom2912@interia.pl>
2016-12-02 13:15:54 +0100

    Created project, added classes and basic fields and methods to them.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,6 @@
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello, Constitution");//FIXME Junk
+        System.out.println("Hello, Constitution");//FIXME This is unk
     }
 }
